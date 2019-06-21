4805152b084e16f283a239fe5e3e1f801f0bbbb2
Johann Asto <johann9000@hotmail.com>
2019-06-20 23:49:06 -0500

    hashtable listo

--- TF_Algoritmos/TF_Algoritmos/HashTable.h
+++ TF_Algoritmos/TF_Algoritmos/HashTable.h
@@ -1,10 +1,40 @@
 #ifndef __HASHTABLE_HPP__
 #define __HASHTABLE_HPP__
+#define TAMAÑO 100
+#include "Lista.hpp"
+#include <vector>
+#include <array>
+using namespace std;
 
 class HashTable {
+	Lista* listas;
+	array<Lista*, TAMAÑO> tabla;
 
+public:
+	HashTable() {
+		listas = new Lista();
+	}
 
+	int funcionHash(string valor) {
+		int suma = 0;
+		for (size_t i = 0; i < valor.size(); i++) {
+			suma += valor.at(i);
+		}
+		return suma % TAMAÑO;
+	}
 
+	void insertar(Fila* nuevo, int ind) {
+		int hash = funcionHash(nuevo->getS(ind));
+		if (tabla[hash] == NULL) {
+			tabla[hash] = new Lista();
+		}
+		tabla[hash]->agregar(nuevo, ind);
+	}
+
+	vector<Fila*>* buscar(string nombre) {
+		int hash = funcionHash(nombre);
+		return tabla[hash]->buscar(nombre);
+	}
 
 };
 
--- TF_Algoritmos/TF_Algoritmos/Lista.hpp
+++ TF_Algoritmos/TF_Algoritmos/Lista.hpp
@@ -13,13 +13,25 @@ public:
 		inicio = fin = nullptr;
 	}
 
-	void agregar(string valor, Fila* elemento) {
-		NodoHT* aux = inicio;
-		for (aux = inicio; aux != fin; aux = aux->getS()) {
-			if (aux->)
+	void agregar(Fila* elemento, int ind) {
+		NodoHT* nuevo = new NodoHT(elemento, ind);
+		if (inicio == nullptr) inicio = nuevo;
+		else if (inicio == fin) {
+			fin->setS(nuevo);
 		}
+		fin = nuevo;
 	}
 
+	vector<Fila*>* buscar(string valor) {
+		vector<Fila*>* filas = new vector<Fila*>;
+		NodoHT* aux;
+		for (aux = inicio; aux->getS(); aux = aux->getS()) {
+			if (valor == aux->getV()) {
+				filas->push_back(aux->getElemento());
+			}
+		}
+		return filas;
+	}
 };
 
 #endif // !__LISTA_HPP__
